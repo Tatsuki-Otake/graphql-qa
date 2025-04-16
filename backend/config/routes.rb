12db2e747be9c570a348b4a80947ae54f2e82416
Ootake <tatsuki.otake@torcheees.com>
2025-04-16 09:25:53 +0900

    feat: mount GraphiQL at root path

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -6,9 +6,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  if Rails.env.development? || Rails.env.production?
+    mount GraphiQL::Rails::Engine, at: "/", graphql_path: "/graphql"
   end
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
