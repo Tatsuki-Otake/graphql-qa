faca75fea091821651866943b8f1e568b0cbfa95
Ootake <tatsuki.otake@torcheees.com>
2025-04-16 09:46:41 +0900

    rollback routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  if Rails.env.development? || Rails.env.production?
+  if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
 end
